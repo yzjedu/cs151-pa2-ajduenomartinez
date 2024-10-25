37ea4c03c2dba3eaa1a0ff0c367a09dd0a8af460
ajduenomartinez <ajduenomartinez@loyola.edu>
2024-10-25 17:13:34 -0400

    initial design

--- final_design.md
+++ final_design.md
@@ -1,2 +1,56 @@
 # Final Design Document
-#### PLEASE! PLEASE! PLEASE! Complete all the NOs in the feedback document 
+
+1. output program explanation to user 
+2. set times_played to 0
+3. set player_number to 0
+4. set player_1_losses to 0
+5. set player_2_losses to 0
+6. set player_3_losses to 0
+7. set game_running to 0
+7. set SENTINEL to 0
+8. while SENTINEL does not equal 1
+   1. if times_played is greater than 0:
+      1. ask user if they want to play again
+      2. if user answers no
+         1. output player_1_losses
+         2. if player_number greater than 0
+            1. output player_2_losses
+         3. output player_3_losses
+         4. exit program
+   2. ask user to input how many players are playing
+      1. while user input doesn't equal "1" or "2" or "one" or "two"
+         1. ask user to input valid number of players
+         2. assign user input for number of players to player_number
+         3. ask user number to enter number of sticks
+            1. while user input isn't greater than 10 or less than 100:
+               1. ask user to input valid number
+         4. assign user input for number of sticks to stick_total 
+         5. output to player that game is starting.
+            1. while total sticks is less than or equal to 0:
+               1. player 1 is asked to input how many sticks they wish to remove.
+                  1. while input is not equal to 1 or 2 or 3:
+                     1. ask user to input valid number
+                  2. user input is subtracted from stick_total
+                  3. if stick total is less than or equal to 0
+                     1. output prompt saying player 1 wins
+                     2. If player 1 loses, 
+                        1. add 1 to player_1_losses
+                     3. set SENTINEL to 1
+                  4. if player number is equal to 2:
+                     1. player 2 is asked to input how many sticks they wish to remove.
+                        1. while input is not equal to 1 or 2 or 3:
+                           1. ask user to input valid number
+                     2. user input is subtracted from stick_number
+                     3. if stick total is less than or equal to 0
+                        1. output prompt saying player 2 wins
+                        2. if player 2 lost:
+                           1. add 1 to player_2_losses
+                        3. set SENTINEL to 1
+                  5. computer player subtracts number. number decided via random number generator set to generate number between 1 and 3. 
+                  6. computer player's number is subtracted from stick_total
+                  7. if stick total is less than or equal to 0
+                     1. output prompt saying player 3 wins
+                     2. if player 3 loses:
+                        1. add 1 to player_3_losses
+                     3. set SENTINEL to 1
+   
--- main.py
+++ main.py
@@ -1,1 +1,106 @@
-# Code goes here and DO NOT FORGET INTRO COMMENTS
+# Programmers: Antonio Dueno
+# Course: CS151, Zelalem Yalew
+# Due Date: 10/23/2024
+# Programming Assignment: 2
+# Problem Statement:You are creating a three player game of the Game of Sticks where players alternate turns
+#                   taking 1-3 sticks. The player to take the last stick loses.
+# Data In: Player input for if player 2 is participating, number of sticks to take from, number of sticks taken by
+#           player 1 and player 2, Player input for if player wishes to play game again.
+# Data Out: Program explanation, number of sticks remaining in a game, player 1 losses, player 2 losses, and player 3 losses.
+# Credits: Class discussion of while loops and SENTINEL.
+
+import random
+
+#Variable values being set before loop
+times_played = 0
+player_number = 0
+player_1_losses = 0
+player_2_losses = 0
+player_3_losses = 0
+game_running = 0
+SENTINEL = 0
+
+#program explanation
+print('Hello! Welcome to the Game of Sticks! In this game, you will enter how many sticks you wish \nto play with,'
+          'then you will take turns with the computer player to remove 1 to 3 sticks. \nWhoever removes the last stick loses!')
+
+#main loop
+while SENTINEL != 1:
+    if times_played > 0:
+        total_sticks = 0
+        game_running = 0
+        replay_game = str(input('Would you like to play again? \nInput "y" for yes and "n" for no:' ))
+        while replay_game != 'y' and replay_game != 'n':
+            replay_game = str(input('Would you like to play again? \n') )
+        if replay_game.lower() == 'n':
+           SENTINEL = 1
+
+    #prompt asking how many players will play
+    player_number = input("How many players will play? Input '1' for you and the computer, and input '2' for you, a friend, and the computer!:")
+    while not player_number.isdigit() and player_number < 1 or player_number > 2:
+        player_number = (input('Please enter a valid number:'))
+
+
+
+
+    #prompt asking how many sticks will be used
+    total_sticks = input('How many sticks will play? Please input a number between 10 and 100:' )
+
+    while total_sticks.isdigit():
+        total_sticks = input('Please enter a valid number:')
+        if total_sticks.isdigit():
+            total_sticks = int(total_sticks)
+            if 10 > total_sticks or total_sticks > 100:
+                total_sticks = input('Please enter a valid number:')
+
+
+    #loop for while game is running
+    while game_running == 0:
+
+        #player 1's turn
+        player_1_sticks = input("Player 1's turn! How many sticks will you remove? Please input a number between 1 and 3:" )
+        while player_1_sticks != 1 or 2 or 3 and not player_1_sticks.isalpha():
+            player_1_sticks = input("Please input a valid number between 1 and 3:" )
+            if player_1_sticks.isdigit():
+                player_1_sticks = int(player_1_sticks)
+            total_sticks = total_sticks - player_1_sticks
+
+        #player 1 loses
+        if total_sticks <= 0:
+            print("Player 1 drew the last stick! You lose, Player 1!")
+            player_1_losses += 1
+            times_played += 1
+            game_running = 1
+
+        #Player 2's turn, if there is a player 2 participating
+        if player_number == 2:
+            player_2_sticks = input("Player 2's turn! How many sticks will you remove? Please input a number between 1 and 3:" )
+            while player_2_sticks != 1 or 2 or 3 and player_2_sticks.isalpha():
+                player_2_sticks = input("Please input a valid number between 1 and 3:" )
+                if player_2_sticks.isdigit():
+                    player_2_sticks = int(player_2_sticks)
+            total_sticks = total_sticks - player_2_sticks
+
+            #Player 2 loses
+            if total_sticks <= 0:
+                print("Player 2 drew the last stick! You lose, Player 2!")
+                player_2_losses += 1
+                times_played += 1
+                game_running = 1
+
+        #Computer player's turn
+        player_3_sticks = random.randint(1, 3)
+        print("COM Player's turn! They removed" + str(player_3_sticks) + " stick(s)!")
+        total_sticks = total_sticks - player_3_sticks
+        if total_sticks <= 0:
+            print("COM player drew the last stick! You lose, COM player!")
+            player_3_losses += 1
+            times_played += 1
+            game_running = 1
+
+print("Thank you for playing the Game of Sticks! Here are the final stats:"
+      "\nGame was played" + str(times_played) + " time(s)!"
+      "\nPlayer 1 lost:", player_1_losses, "time(s)!"
+      "\nPlayer 2 lost:", player_2_losses, "time(s)!"
+      "\nPlayer 3 lost:", player_3_losses, "time(s)!")
+
